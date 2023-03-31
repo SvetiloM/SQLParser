0a947cf2568163d1a706ae64c616aa61a0276f84
svetlanamorgunova <nomail@gm.ru>
2023-04-01 01:09:24 +0400

    Add parsing inner selects cache

--- src/main/java/sm/sql/parser/ParseService.java
+++ src/main/java/sm/sql/parser/ParseService.java
@@ -2,7 +2,7 @@ package sm.sql.parser;
 
 import lombok.RequiredArgsConstructor;
 import sm.sql.parser.entity.Select;
-import sm.sql.parser.parser.InnerQueryParser;
+import sm.sql.parser.parser.InnerQueryCache;
 import sm.sql.parser.parser.SelectQueryParser;
 
 import java.util.Map;
@@ -10,14 +10,11 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 public class ParseService {
-
-    private final Map<String, Select> selectCashe;
-    private final InnerQueryParser innerQueryParser;
+    private final InnerQueryCache innerQueryParser;
     private final SelectQueryParser selectQueryParser;
 
     public Select parse(String s) {
-        Optional<Map<String, Select>> parse = innerQueryParser.parse(s);
-        parse.ifPresent(selectCashe::putAll);
+        innerQueryParser.fillCache(s);
 
         return selectQueryParser.parse(s).orElseGet(Select::new);
     }
--- src/main/java/sm/sql/parser/parser/InnerQueryCache.java
+++ src/main/java/sm/sql/parser/parser/InnerQueryCache.java
@@ -3,33 +3,26 @@ package sm.sql.parser.parser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import sm.sql.parser.entity.Select;
-import sm.sql.parser.entity.part.CollectionPartType;
-import sm.sql.parser.entity.part.Part;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
-public class InnerQueryParser implements Parser<Map<String, Select>> {
+public class InnerQueryCache {
 
     private final BracketParser bracketParser = new BracketParser();
     private final SelectQueryParser selectQueryParser;
+    private final Map<String, Select> selectCache;
 
-    @Override
-    public Optional<Map<String, Select>> parse(String s) {
-        Map<String, Select> selectMap = new HashMap<>();
+    public void fillCache(String s) {
         Optional<List<String>> optional = bracketParser.parse(s);
         if (optional.isPresent()) {
             List<String> parts = optional.get();
+            parts.sort(Comparator.comparingInt(String::length));
             for (String part : parts) {
-                selectQueryParser.parse(part).ifPresent(select -> selectMap.put(addBracket(part), select));
+                selectQueryParser.parse(part).ifPresent(select -> selectCache.put(addBracket(part), select));
             }
         }
-
-        return Optional.of(selectMap);
     }
 
     private String addBracket(String s) {
